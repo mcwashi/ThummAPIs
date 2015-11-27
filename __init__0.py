d0184b5050505fdfeb5c1bbda976a0b050b133f7
Marcus Washington <marcus.washington@me.com>
2015-11-27 00:13:47 -0600

    Thumm APIs

---
+++ __init__0.py
@@ -0,0 +1,18 @@
+{
+    'id': 4,
+    'name': u'Buy groceries',
+    'username': '',
+    'email': 'mike@doe.com',
+    'password': 'testpassword',
+    'facebook_id': '',
+    'location': 'Los Angelos, United States',
+    'image': 'shower_in_thunderstorm_300.jpg',
+    'key': '',
+    'status': '1',
+    'genre': 'm',
+    'date_added':'2014-02-12 00:00:00',
+    'mode': '0',
+    'last_login':'2014-02-13 13:58:16',
+    'mobile': '+4023432423423',
+    'token': '4cds7cr6o@1392299947ym'
+}
