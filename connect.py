6181ddeaac3b0d7d0b010f69b7b58c4ebab34506
Marcus Washington <marcus.washington@me.com>
2015-12-29 11:53:09 -0600

    New Changes.

--- connect.py
+++ connect.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:sucram#4963@localhost/Thumm'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/Thumm'
 db = SQLAlchemy(app)
 
 class Users(db.Model):
     __tablename__ = 'User1'
     id = db.Column('id', db.Integer, primary_key=True)
     User = db.Column('User', db.Unicode)
+
+    def __init__(self, id, User):
+        self.id = id
+        self.User = User
