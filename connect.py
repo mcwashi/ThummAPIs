fb309cedfb55b7bbb40dbde9d0b093c92b7cab51
Marcus Washington <marcus.washington@me.com>
2015-12-29 16:49:54 -0600

    Moving on up

--- connect.py
+++ connect.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/Thumm'
+app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
 
 class Users(db.Model):
--- one_to_many.py
+++ one_to_many.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.restless import APIManager
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
+app.run(host='74.207.235.39')
 db = SQLAlchemy(app)
 
 class Person(db.Model):
@@ -14,3 +16,11 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+manager = APIManager(app, flask_sqlalchemy_db=db)
+
+manager.create_api(Person)
+manager.create_api(Pet)
+
+if __name__ == "__main__":
+    app.run(debug=True)
